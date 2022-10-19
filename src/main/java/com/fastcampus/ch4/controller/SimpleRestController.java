45a634bb929ebb4ac51244d314810cde4710c10c
hyejin222 <hyejin.dev@gmail.com>
2022-10-19 23:58:14 +0900

    baord - comment end

--- src/main/java/com/fastcampus/ch4/controller/SimpleRestController.java
+++ src/main/java/com/fastcampus/ch4/controller/SimpleRestController.java
@@ -4,10 +4,14 @@ import com.fastcampus.ch4.domain.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class SimpleRestController {
 //    @GetMapping("/ajax")
 //    public String ajax() { return "ajax"; }
+
+
+    @GetMapping("/test")
+    public String test() { return "test"; }
 
     @PostMapping("/send")
 //    @ResponseBody
@@ -15,16 +19,6 @@ public class SimpleRestController {
         System.out.println("p = " + p);
         p.setName("ABC");
         p.setAge(p.getAge() + 10);
-
-        return p;
-    }
-
-    @PostMapping("/send")
-//    @ResponseBody
-    public Person test2(@RequestBody Person p) {
-        System.out.println("p = " + p);
-        p.setName("GG");
-        p.setAge(p.getAge() + 20);
 
         return p;
     }
